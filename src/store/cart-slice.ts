b0c1b6c600b6b43737b761305c8810beaeafba79
Ali ihsan Ertugrul <ertugrulali3538@gmail.com>
2024-08-05 23:39:45 +0300

    creating reducers functions

---
+++ src/store/cart-slice.ts
@@ -0,0 +1,16 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+export const cartSlice = createSlice({
+  name: 'counter',
+  initialState: {
+    value: 0,
+  },
+  reducers: {
+   
+  },
+})
+
+// Action creators are generated for each case reducer function
+export const { } = cartSlice.actions
+
+export default cartSlice.reducer
